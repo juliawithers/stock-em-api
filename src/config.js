10d27168684a1f8250ad64ba08a61a23d9752ac7
Julia Fehr <09jwithers@gmail.com>
2020-06-30 11:10:27 -0400

    update to config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: "postgresql://postgres@localhost/stock-em",
-    CLIENT_ORIGIN: "https://stock-em.vercel.app/"
-    // DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/stock-em",
+    CLIENT_ORIGIN: "https://stock-em.vercel.app/",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/stock-em",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/stock-em-test",
 }
